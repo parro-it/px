3b2a0c1be30a92825e82184abf2c8c1da824cabf
Andrea Parodi <andrea@parro.it>
2017-12-11 17:49:27 +0100

    more tests

--- test/test-cli.js
+++ test/test-cli.js
@@ -12,3 +12,31 @@ test("run a command", async t => {
     `${__dirname}\n${__dirname}/test-cli.js\n${__dirname}/test-runtime.js`
   );
 });
+
+test("use npm run", async t => {
+  const proc = await execa("npm", ["run", "test"], {
+    cwd: `${__dirname}/../examples`
+  });
+  t.is(proc.stdout, `it work!`);
+});
+
+test("semicolon", async t => {
+  const proc = await execa("npm", ["run", "semicolon"], {
+    cwd: `${__dirname}/../examples`
+  });
+  t.is(proc.stdout, `1\n2`);
+});
+
+test("logical and", async t => {
+  const proc = await execa("npm", ["run", "and"], {
+    cwd: `${__dirname}/../examples`
+  });
+  t.is(proc.stdout, `1\n2`);
+});
+
+test("logical or", async t => {
+  const proc = await execa("npm", ["run", "or"], {
+    cwd: `${__dirname}/../examples`
+  });
+  t.is(proc.stdout, `1\n2`);
+});
