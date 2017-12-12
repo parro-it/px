0ea03d3bf31322827b27aec246c168688cc72533
Andrea Parodi <andrea@parro.it>
2017-12-12 22:48:43 +0100

    verbose

--- test/test-cli.js
+++ test/test-cli.js
@@ -15,7 +15,7 @@ test("run a command", async t => {
 
 async function runNpm(...args) {
   try {
-    const proc = await execa("npm", ["--silent", "run"].concat(args), {
+    const proc = await execa("npm", ["run"].concat(args), {
       cwd: `${__dirname}/../examples`
     });
     console.error(proc.stderr);
